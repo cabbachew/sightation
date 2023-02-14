c62d0c5cf2947a6e31d37bef981c6ca9b97dd9ec
cabbachew <102335309+cabbachew@users.noreply.github.com>
2023-02-13 22:57:37 -0800

    set up text completion using open ai api

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,12 +3,29 @@ class DashboardController < ApplicationController
   end
 
   def query
+    open_ai_completion
     puts "PARAMS: #{params.inspect}"
     respond_to do |format|
       format.json { render json: { answer: "Question: #{params[:question]}, Source: #{params[:source]}" } }
     end
   end
 
+  private
+
   # TODO: Add strong parameters
 
+  def open_ai_client
+    @client ||= OpenAI::Client.new(access_token: ENV.fetch('OPEN_AI_KEY'))
+  end
+
+  def open_ai_completion
+    response = open_ai_client.completions(
+      parameters: {
+          model: "text-ada-001",
+          prompt: "Once upon a time",
+          max_tokens: 5
+      })
+    puts response
+    # puts response["choices"].map { |c| c["text"] }
+  end
 end
